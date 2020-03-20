ef92b630da572f346572d50b5aca11bbdc99d7f3
jennyzhu <jenny.zhu84@gmail.com>
2020-03-20 02:48:25 -0400

    add corona raining animation

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function strop(cleft, ctop, d) {
+    var drop = document.createElement('img');
+    drop.src = "https://cdn.smassets.net/assets/cms/cc/uploads/taller-header-coronavirus-resources.png"
+    drop.className = 'punct';
+    drop.style.left = cleft + 'px';
+    drop.style.top = ctop + 'px';
+    drop.id = d;
+    document.getElementById('content').appendChild(drop);
+}
+
+function randomFromInterval(from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+}
+var n, interval;
+
+function newDrop() {
+    var x = randomFromInterval(20, window.innerWidth),
+        y = randomFromInterval(10, 100);
+    strop(x, y, n);
+    n--;
+    if (n > 0) {
+        setTimeout(newDrop, 500);
+        // 500ms is the interval between drops appearing
+    }
+}
+
+window.onload = function() {
+    n = 100;
+    newDrop();
+    interval = setInterval(function() {
+        var drops = document.getElementsByClassName('punct'),
+            newY;
+        if (drops.length == 0) {
+            clearInterval(interval);
+            return;
+        }
+        for (var i = 0; i < drops.length; i++) {
+            newY = drops[i].offsetTop + 20;
+            if (newY > drops[i].parentNode.offsetHeight) {
+                drops[i].parentNode.removeChild(drops[i]);
+            }
+            else {
+                drops[i].style.top = newY + 'px';
+            }
+        }
+    }, 30);
+}
